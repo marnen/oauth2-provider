f6302933eea2d4345e472655de1516427340820b
Kareem Kouddous <kareem@doubleonemedia.com>
2012-09-26 18:24:26 -0400

    Updating for Mongoid 3

--- lib/oauth2/provider/models/mongoid.rb
+++ lib/oauth2/provider/models/mongoid.rb
@@ -22,9 +22,9 @@ module OAuth2::Provider::Models::Mongoid
     OAuth2::Provider.authorization_code_class_name ||= "OAuth2::Provider::Models::Mongoid::AuthorizationCode"
     OAuth2::Provider.authorization_class_name ||= "OAuth2::Provider::Models::Mongoid::Authorization"
 
-    OAuth2::Provider.client_class.collection_name = client_collection_name
-    OAuth2::Provider.access_token_class.collection_name = access_token_collection_name
-    OAuth2::Provider.authorization_code_class.collection_name = authorization_code_collection_name
-    OAuth2::Provider.authorization_class.collection_name = authorization_collection_name
+    OAuth2::Provider.client_class.store_in(:collection => client_collection_name)
+    OAuth2::Provider.access_token_class.store_in(:collection => access_token_collection_name)
+    OAuth2::Provider.authorization_code_class.store_in(:collection => authorization_code_collection_name)
+    OAuth2::Provider.authorization_class.store_in(:collection => authorization_collection_name)
   end
 end
